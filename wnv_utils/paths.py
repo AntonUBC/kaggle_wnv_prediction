61f4847c064fc6fe41a7f2875abdaf1905c6f00b
AntonUBC <laptiev.anton@gmail.com>
2015-11-05 17:19:28 -0800

    Update paths.py

--- wnv_utils/paths.py
+++ wnv_utils/paths.py
@@ -6,7 +6,7 @@ PROJECT_PATH = "D:/Data_Science_Projects/WNV/"
 
 # data and submission folders
 DATA_PATH = os.path.join(PROJECT_PATH, "data")
-SUBMISSION_PATH = os.path.join(PROJECT_PATH, "submission/")
+SUBMISSION_PATH = os.path.join(PROJECT_PATH, "submission")
 DATA_TRAIN_PATH = os.path.join(DATA_PATH, "train.csv")
 DATA_TEST_PATH = os.path.join(DATA_PATH, "test.csv")
 DATA_WEATHER_PATH = os.path.join(DATA_PATH, "weather.csv")
